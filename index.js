2175d15dad3e44a5bca2cacc8b8dca332921aed4
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2020-09-21 16:46:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+$("#one1").click(function () {
+  let one = $("#one1").html();
+  let get = $(".result").html();
+  $(".result").html(parseInt(one) + parseInt(get));
+});
+
+$("#one2").click(function () {
+  let one = $("#one2").html();
+  let get = $(".result").html();
+  $(".result").html(parseInt(one) + parseInt(get));
+});
+
+$("#one3").click(function () {
+  let one = $("#one3").html();
+  let get = $(".result").html();
+  $(".result").html(parseInt(one) + parseInt(get));
+});
+
+$("#one-1").click(function () {
+  let one = $("#one-1").html();
+  let get = $(".result").html();
+  $(".result").html(parseInt(one) + parseInt(get));
+});
+
+$("#one-2").click(function () {
+  let one = $("#one-2").html();
+  let get = $(".result").html();
+  $(".result").html(parseInt(one) + parseInt(get));
+});
+
+$("#one-3").click(function () {
+  let one = $("#one-3").html();
+  let get = $(".result").html();
+  $(".result").html(parseInt(one) + parseInt(get));
+});
